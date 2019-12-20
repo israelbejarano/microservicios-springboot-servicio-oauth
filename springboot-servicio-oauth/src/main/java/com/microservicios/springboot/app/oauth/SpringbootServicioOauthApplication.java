e904dcbbd5de10210fa1d7ff27422885d0f68309
israel <israel.bejarano.a@gmail.com>
2019-12-20 22:31:51 +0100

    @EnableEurekaClient

--- springboot-servicio-oauth/src/main/java/com/microservicios/springboot/app/oauth/SpringbootServicioOauthApplication.java
+++ springboot-servicio-oauth/src/main/java/com/microservicios/springboot/app/oauth/SpringbootServicioOauthApplication.java
@@ -2,8 +2,10 @@ package com.microservicios.springboot.app.oauth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class SpringbootServicioOauthApplication {
 
 	public static void main(String[] args) {
